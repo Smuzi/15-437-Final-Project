59e9939b889e49a9de39c291db56b1e34bf9a6dd
Jacob Olson <jakster5487@gmail.com>
2012-04-24 14:58:43 -0400

    Okay, last time I swear, I redid denying access to jsp's. I moved them to the WEB-INF folder, which makes the server deny any existence of them, rather than returning a "forbidden" message like it did before. Note that when returning jsp's from Actions, the base name of the jsp can still be returned, the "WEB-INF" automatically gets appended.

--- WEB-INF/src/controller/Controller.java
+++ WEB-INF/src/controller/Controller.java
@@ -117,7 +117,8 @@ public class Controller extends HttpServlet
 
         if (nextPage.endsWith(".jsp"))
         {
-            RequestDispatcher d = request.getRequestDispatcher(nextPage);
+            RequestDispatcher d =
+                request.getRequestDispatcher("WEB-INF/" + nextPage);
             d.forward(request, response);
             return;
         }
