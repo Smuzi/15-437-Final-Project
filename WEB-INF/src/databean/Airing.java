d549256734d7977de1191285c548ba630d353a22
Jacob Olson <jakster5487@gmail.com>
2012-04-25 19:18:01 -0400

    Added some fields to Airing databean

--- WEB-INF/src/databean/Airing.java
+++ WEB-INF/src/databean/Airing.java
@@ -18,25 +18,32 @@ public class Airing
     private int id;
     private int showId;
     private int providerId;
-    private Date airTime;
+    private Date startTime;
+    private Date stopTime;
+    private int episodeNumber;
     private int channelNumber;
     private String channelName;
 
     /* Getters */
-    public int getId()          { return id; }
-    public int getShowId()      { return showId; }
-    public int getProviderId()  { return providerId; }
-    public Date getAiringTime() { return airTime; }
-    public int getChannelNumber() { return channelNumber; }
+    public int getId()             { return id; }
+    public int getShowId()         { return showId; }
+    public int getProviderId()     { return providerId; }
+    public Date getStartTime()     { return startTime; }
+    public Date getStopTime()      { return stopTime; }
+    public int getEpisodeNumber()  { return episodeNumber; }
+    public int getChannelNumber()  { return channelNumber; }
     public String getChannelName() { return channelName; }
 
     /* Setters */
-    public void setId(int id)                 { this.id = id; }
-    public void setShowId(int showId)         { this.showId = showId; }
+    public void setId(int id)                 { this.id         = id; }
+    public void setShowId(int showId)         { this.showId     = showId; }
     public void setProviderId(int providerId) { this.providerId = providerId; }
-    public void setDescription(Date airTime)  { this.airTime = airTime; }
+    public void setStartTime(Date startTime)  { this.startTime  = startTime; }
+    public void setStopTime(Date stopTime)    { this.stopTime   = stopTime; }
+    public void setEpisodeNumber(int episodeNumber)
+                                        { this.episodeNumber = episodeNumber; }
     public void setChannelNumber(int channelNumber)
                                         { this.channelNumber = channelNumber; }
     public void setChannelName(String channelName)
-                                        { this.channelName = channelName; }
+                                        { this.channelName   = channelName; }
 }
